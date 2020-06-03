4ee0ba102a2f1c4db56ae44edb9c5ca4ac202427
OursCat <jianzhang0813@gmail.com>
2020-06-03 01:05:31 -0400

    initial project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//run when client connects
+
+io.on('connection', (socket) => {
+	console.log('new connection...');
+});
+
+const port = proc
